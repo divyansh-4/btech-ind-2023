af5d0ca6b8dbd31ab6e0e4203dbe4ed3210cd303
aryanteng <aryanteng892@gmail.com>
2023-07-11 22:25:04 +0530

    created seo component for the platform

---
+++ components/common/SEO.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { NextSeo } from 'next-seo';
+import { BASE_URL } from '../../utils/baseUrl';
+
+function SEO({ title, desc, imgSrc, url }) {
+  const imageSrc = imgSrc ?? '/vercel.svg'; // we need a logo svg
+  const description = desc ?? 'IIITD Induction 2023'; // we need description content
+  const seoTitle = title ?? 'IIITD Induction 2023';
+  const seoUrl = url ?? BASE_URL;
+  return (
+    <NextSeo
+      title={seoTitle}
+      defaultTitle='IIITD Induction 2023'
+      description={description}
+      additionalMetaTags={[
+        {
+          property: 'og:title',
+          content: seoTitle,
+        },
+        {
+          property: 'og:image',
+          itemprop: 'image',
+          content: imageSrc,
+        },
+        {
+          property: 'og:description',
+          content: description,
+        },
+        {
+          property: 'og:site_name',
+          content: 'IIITD Induction 2023',
+        },
+        {
+          itemprop: 'thumbnailUrl',
+          href: imageSrc,
+        },
+        {
+          property: 'og:type',
+          content: 'website',
+        },
+        {
+          property: 'og:url',
+          content: seoUrl,
+        },
+        {
+          itemProp: 'image',
+          content: imageSrc,
+        },
+        {
+          itemProp: 'description',
+          content: description,
+        },
+        {
+          name: 'msapplication-TileImage',
+          content: logoSrc,
+        },
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1.0',
+        },
+      ]}
+      additionalLinkTags={[
+        {
+          rel: 'icon',
+          href: '/favicon.svg',
+        },
+        {
+          rel: 'apple-touch-icon',
+          href: '/favicon.svg',
+          sizes: '76x76',
+        },
+      ]}
+    />
+  );
+}
+
+export default SEO;
