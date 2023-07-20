b68b024b40b88108db840522f492aca6eb7c8918
aryanteng <aryanteng892@gmail.com>
2023-07-20 21:40:01 +0530

    changes to styles

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,13 +1,14 @@
 import { Menu } from '@mui/icons-material';
 import Image from 'next/image';
-import React from 'react';
+import React, { useMemo } from 'react';
 
 function Header() {
+  const iconStyle = useMemo(() => 'w-5 md:w-7', []);
   return (
     <div className='w-full bg-red sticky'>
-      <div className='flex items-center justify-between py-2 px-10'>
+      <div className='flex items-center justify-between py-2 px-4 md:px-5 lg:px-10'>
         <div>
-          <Menu className='text-white text-3xl' />
+          <Menu className='text-white text-2xl md:text-3xl' />
         </div>
         <Image width={125} height={125} src='/induction.svg' alt='IIITD Logo' />
         <div className='flex items-center gap-5'>
@@ -16,18 +17,21 @@ function Header() {
             height={25}
             src='/icons/instagram.svg'
             alt='IIITD Logo'
+            className={iconStyle}
           />
           <Image
             width={25}
             height={25}
             src='/icons/linkedin.svg'
             alt='IIITD Logo'
+            className={iconStyle}
           />
           <Image
             width={25}
             height={25}
             src='/icons/twitter.svg'
             alt='IIITD Logo'
+            className={iconStyle}
           />
         </div>
       </div>
