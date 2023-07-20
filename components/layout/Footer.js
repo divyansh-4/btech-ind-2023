557c03497b322f1e730c4028461494da7d26cd77
aryanteng <aryanteng892@gmail.com>
2023-07-20 22:04:21 +0530

    additional breakpoints

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -38,7 +38,7 @@ function Footer() {
     [infoListTextStyle]
   );
   return (
-    <div className='w-full bottom-0 absolute bg-[#252525] p-4 sm:p-8 md:p-10 flex flex-col md:flex-row justify-between text-red gap-5'>
+    <div className='w-full bottom-0 absolute bg-[#252525] p-4 sm:p-6 md:p-8 lg:p-10 flex flex-col md:flex-row justify-between text-red gap-5'>
       <div className='flex flex-col gap-2'>
         <div className='flex items-center gap-2'>
           <Image width={75} height={75} src='/logo.svg' alt='IIITD Logo' />
