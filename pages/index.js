7719feba5894aa49656cb732534dd6446d7c8de6
Paras Verma <workparas8800@gmail.com>
2023-07-23 22:22:47 +0530

    added keynote section

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import SEO from '../components/common/SEO';
 import Body from '../components/layout/Body';
 import Image from 'next/image';
 import Parallax from '../components/common/Parallax';
+import Keynotesec from '../components/Keynotesec'
 
 export default function Home() {
   return (
@@ -18,11 +19,12 @@ export default function Home() {
                 factor={4}
               >
                 <div
-                  className='bg-repeat-y bg-top bg-cover md:bg-[length:100%] w-full h-full'
+                  className='bg-repeat-y bg-cover md:bg-[length:100%]   w-full h-full bg-top'
+                  // className='bg-top bg-repeat-y bg-top bg-[length:100%] w-full h-full'
                   style={{
                     backgroundImage: "url('/stickerwall.png')",
                   }}
-                ></div>
+                  ></div>
               </Parallax>
               <div className='flex justify-center items-center h-screen'>
                 <TitleCard
@@ -49,8 +51,25 @@ export default function Home() {
             </div>
           </>
         }
-        secondSection={<div className='h-screen'></div>}
-        thirdSection={<div className='h-screen'></div>}
+        secondSection={<>
+          <div className='relative h-fit top-[-40vh]  pt-[40vh]'>
+            <Parallax
+              className="absolute top-0 left-0 w-full h-full z-0 "
+              factor={4}
+              >
+              <div
+                // className="bg-cover md:bg-[length:100%]  w-full h-full bg-top  bg-repeat-y "
+                className='bg-top bg-repeat-y bg-[length:100%] w-full h-full'
+                style={{
+                  backgroundImage: "url('/stickerforkeynote.png') ",
+                }}
+              ></div>
+            </Parallax>
+            <Keynotesec/>
+
+          </div>        
+          </>}
+        thirdSection={<div className='h-screen' ></div>}
       />
     </>
   );
