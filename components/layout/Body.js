e6ea3609849bdba26cb8733158a77dff40430dc0
ayugupt <ayush.gupta.10301@gmail.com>
2023-07-24 17:36:37 +0530

    fix: white space

--- components/layout/Body.js
+++ components/layout/Body.js
@@ -32,53 +32,19 @@ export default function Body({ firstSection, secondSection, thirdSection }) {
 
   return (
     <>
-      <div
-        className={`bg-[#DF392E] relative bg-bottom bg-cover bg-no-repeat`}
-        style={{
-          backgroundImage: "url('/background_filters/section1.png')",
-          paddingBottom: `${ripHeight / 1.5}px`,
-        }}
-      >
-        <Header />
-        {firstSection}
+      <div className="relative">
         <div
-          className={`absolute w-full h-[${ripHeight}px] bottom-0 left-0 z-[2]`}
-          style={{ height: `${ripHeight}px` }}
-        >
-          <Image
-            src="/background_filters/ripped1.svg"
-            fill={true}
-            alt="rip"
-            className="object-cover object-center"
-          />
-        </div>
-      </div>
-      <div
-        className="bg-[#32BD6C] relative z-[1] bg-bottom bg-cover bg-no-repeat"
-        style={{
-          top: `-${ripHeight / 1.5}px`,
-          clipPath: `polygon(${clipPathTop}, ${clipPathBottom})`,
-          backgroundImage: "url('/background_filters/section2.png')",
-          paddingTop: `${ripHeight / 1.5}px`,
-        }}
-      >
-        {secondSection}
-      </div>
-      {thirdSection && (
-        <div
-          className="bg-[#DF392E] relative bg-bottom bg-cover bg-no-repeat"
+          className={`bg-[#DF392E] relative bg-bottom bg-cover bg-no-repeat`}
           style={{
-            top: `-${ripHeight + 36}px`,
-            backgroundImage: "url('/background_filters/section3.png')",
-            paddingTop: `${ripHeight / 3}px`,
+            backgroundImage: "url('/background_filters/section1.png')",
+            paddingBottom: `${ripHeight / 1.5}px`,
           }}
         >
+          <Header />
+          {firstSection}
           <div
-            className="absolute w-full z-[3]"
-            style={{
-              height: `${ripHeight}px`,
-              top: `-${ripHeight / 3}px`,
-            }}
+            className={`absolute w-full h-[${ripHeight}px] bottom-0 left-0 z-[2]`}
+            style={{ height: `${ripHeight}px` }}
           >
             <Image
               src="/background_filters/ripped1.svg"
@@ -87,9 +53,49 @@ export default function Body({ firstSection, secondSection, thirdSection }) {
               className="object-cover object-center"
             />
           </div>
-          {thirdSection}
         </div>
-      )}
+        <div
+          className="bg-[#32BD6C] relative z-[1] bg-bottom bg-cover bg-no-repeat"
+          style={{
+            top: `-${ripHeight / 1.5}px`,
+            clipPath: `polygon(${clipPathTop}, ${clipPathBottom})`,
+            backgroundImage: "url('/background_filters/section2.png')",
+            paddingTop: `${ripHeight / 1.5}px`,
+          }}
+        >
+          {secondSection}
+        </div>
+        {thirdSection && (
+          <div
+            className="bg-[#DF392E] relative bg-bottom bg-cover bg-no-repeat"
+            style={{
+              top: `-${ripHeight + 36}px`,
+              backgroundImage: "url('/background_filters/section3.png')",
+              paddingTop: `${ripHeight / 3}px`,
+            }}
+          >
+            <div
+              className="absolute w-full z-[3]"
+              style={{
+                height: `${ripHeight}px`,
+                top: `-${ripHeight / 3}px`,
+              }}
+            >
+              <Image
+                src="/background_filters/ripped1.svg"
+                fill={true}
+                alt="rip"
+                className="object-cover object-center"
+              />
+            </div>
+            {thirdSection}
+          </div>
+        )}
+        <div
+          className={`absolute w-full bg-background`}
+          style={{ height: `${ripHeight + 36}px`, bottom: `0` }}
+        ></div>
+      </div>
       <Footer />
     </>
   );
