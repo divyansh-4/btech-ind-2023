68cdf90fb63c4311c761fa9672ed585e882afd2a
aryanteng <aryanteng892@gmail.com>
2023-07-26 21:53:31 +0530

    menu links added

--- components/common/Menu.js
+++ components/common/Menu.js
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { useMemo } from 'react';
 
 function UnderLineText({ text }) {
   return (
@@ -18,6 +19,24 @@ export default function Menu() {
   const contactResolution = { width: 142, height: 156 };
   const virtualTourResolution = { width: 161, height: 127 };
   const offset = scheduleResolution.height / 2;
+
+  const menuLinks = useMemo(
+    () => [
+      {
+        name: 'About Us',
+        href: 'https://www.iiitd.ac.in/',
+      },
+      {
+        name: 'Location',
+        href: 'https://www.google.com/maps/place/Indraprastha+Institute+of+Information+Technology+Delhi/@28.5458541,77.2731762,17z/data=!3m1!4b1!4m6!3m5!1s0x390ce3e564daac1d:0x2c582e340e7bc556!8m2!3d28.5458541!4d77.2731762!16s%2Fg%2F11bc5q5mqf?entry=ttu',
+      },
+      {
+        name: 'FAQs',
+        href: 'https://docs.google.com/spreadsheets/d/1w85BGovNseeoUiKojVfF8NYx4NBOw_5UfrIc6ALt4Yo/edit#gid=1213820881',
+      },
+    ],
+    []
+  );
 
   return (
     <div className='w-screen h-full sm:h-auto sm:w-fit p-4 bg-yellow rounded-3xl flex flex-col justify-between'>
@@ -56,10 +75,21 @@ export default function Menu() {
       <div style={{ height: `${offset}px` }}></div>
       <div className='mt-2 flex items-end justify-between'>
         <div className='leading-tight font-bold'>
-          <UnderLineText text='Home' />
-          <UnderLineText text='About Us' />
-          <UnderLineText text='Location' />
-          <UnderLineText text='FAQs' />
+          <Link href={'/'} className='text-black no-underline'>
+            <UnderLineText text='Home' />
+          </Link>
+          {menuLinks.map((menuItem) => {
+            return (
+              <a
+                key={menuItem.href}
+                className='text-black no-underline'
+                href={menuItem.href}
+                target='_blank'
+              >
+                <UnderLineText text={menuItem.name} />
+              </a>
+            );
+          })}
         </div>
         <div className='w-10 h-10 rounded-full bg-background'></div>
       </div>
