2a238aad0c53df1b5c2acd3524468db918180579
aryanteng <aryanteng892@gmail.com>
2023-07-28 22:39:51 +0530

    removing unused imports

--- components/common/Menu.js
+++ components/common/Menu.js
@@ -1,20 +1,19 @@
-import Image from "next/image";
-import Link from "next/link";
-import { useMemo } from "react";
+import Image from 'next/image';
+import Link from 'next/link';
+import { useMemo } from 'react';
 
 function UnderLineText({ text }) {
   return (
-    <div className="relative w-fit cursor-pointer [&>div]:hover:w-full">
+    <div className='relative w-fit cursor-pointer [&>div]:hover:w-full'>
       <span>{text}</span>
-      <div className="absolute bottom-0 w-0 h-[2px] transition-all duration-100 bg-black"></div>
+      <div className='absolute bottom-0 w-0 h-[2px] transition-all duration-100 bg-black'></div>
     </div>
   );
 }
 
 export default function Menu() {
-  // hover:scale-110 hover:rotate-12
   const menuItem =
-    "flex-grow-0 flex-shrink-1 basis-[50%] flex justify-center transiton-all duration-100 cursor-pointer";
+    'flex-grow-0 flex-shrink-1 basis-[50%] flex justify-center transiton-all duration-100 cursor-pointer';
   const scheduleResolution = { width: 199, height: 120 };
   const iccResolution = { width: 177, height: 140 };
   const contactResolution = { width: 176, height: 178 };
@@ -24,16 +23,16 @@ export default function Menu() {
   const menuLinks = useMemo(
     () => [
       {
-        name: "About Us",
-        href: "https://www.iiitd.ac.in/",
+        name: 'About Us',
+        href: 'https://www.iiitd.ac.in/',
       },
       {
-        name: "Location",
-        href: "https://www.google.com/maps/place/Indraprastha+Institute+of+Information+Technology+Delhi/@28.5458541,77.2731762,17z/data=!3m1!4b1!4m6!3m5!1s0x390ce3e564daac1d:0x2c582e340e7bc556!8m2!3d28.5458541!4d77.2731762!16s%2Fg%2F11bc5q5mqf?entry=ttu",
+        name: 'Location',
+        href: 'https://www.google.com/maps/place/Indraprastha+Institute+of+Information+Technology+Delhi/@28.5458541,77.2731762,17z/data=!3m1!4b1!4m6!3m5!1s0x390ce3e564daac1d:0x2c582e340e7bc556!8m2!3d28.5458541!4d77.2731762!16s%2Fg%2F11bc5q5mqf?entry=ttu',
       },
       {
-        name: "FAQs",
-        href: "https://docs.google.com/spreadsheets/d/1w85BGovNseeoUiKojVfF8NYx4NBOw_5UfrIc6ALt4Yo/edit#gid=1213820881",
+        name: 'FAQs',
+        href: 'https://docs.google.com/spreadsheets/d/1w85BGovNseeoUiKojVfF8NYx4NBOw_5UfrIc6ALt4Yo/edit#gid=1213820881',
       },
     ],
     []
@@ -41,72 +40,72 @@ export default function Menu() {
 
   return (
     <div
-      className="w-screen h-full sm:h-auto sm:w-fit p-4 bg-yellow flex flex-col justify-between bg-cover bg-center bg-no-repeat"
+      className='w-screen h-full sm:h-auto sm:w-fit p-4 bg-yellow flex flex-col justify-between bg-cover bg-center bg-no-repeat'
       style={{ backgroundImage: "url('/menu/bg_texture.png')" }}
     >
       <div>
-        <span className="rounded-2xl bg-red text-background p-1 border-4 border-background border-solid text-xs sm:text-sm font-bold w-fit">
+        <span className='rounded-2xl bg-red text-background p-1 border-4 border-background border-solid text-xs sm:text-sm font-bold w-fit'>
           DISCOVER MORE
         </span>
-        <div className="text-6xl sm:text-8xl font-bold">MENU</div>
+        <div className='text-6xl sm:text-8xl font-bold'>MENU</div>
       </div>
-      <div className="flex items-center">
+      <div className='flex items-center'>
         <div className={`${menuItem}`}>
-          <Link href="/schedule">
+          <Link href='/schedule'>
             <Image
-              alt="schedule"
-              src="/menu/schedule.png"
+              alt='schedule'
+              src='/menu/schedule.png'
               {...scheduleResolution}
             />
           </Link>
         </div>
         <div className={`${menuItem} relative`} style={{ top: `${offset}px` }}>
           <Link
-            href="http://studentcouncil.iiitd.edu.in/clubs.html"
-            target="_blank"
+            href='http://studentcouncil.iiitd.edu.in/clubs.html'
+            target='_blank'
           >
-            <Image alt="clubs" src="/menu/clubs.png" {...clubsResolution} />
+            <Image alt='clubs' src='/menu/clubs.png' {...clubsResolution} />
           </Link>
         </div>
       </div>
-      <div className="flex items-center">
+      <div className='flex items-center'>
         <div className={`${menuItem}`}>
           <Link
-            href="https://iiitd.ac.in/life/discipline-grievance/anti-sexual-harassment-committee"
-            target="_blank"
+            href='https://iiitd.ac.in/life/discipline-grievance/anti-sexual-harassment-committee'
+            target='_blank'
           >
-            <Image alt="icc" src="/menu/icc.png" {...iccResolution} />
+            <Image alt='icc' src='/menu/icc.png' {...iccResolution} />
           </Link>
         </div>
         <div className={`${menuItem} relative`} style={{ top: `${offset}px` }}>
-          <Link href="https://www.iiitd.ac.in/contact" target="_blank">
+          <Link href='https://www.iiitd.ac.in/contact' target='_blank'>
             <Image
-              alt="contact"
-              src="/menu/contact.png"
+              alt='contact'
+              src='/menu/contact.png'
               {...contactResolution}
             />
           </Link>
         </div>
       </div>
       <div style={{ height: `${offset}px` }}></div>
-      <div className="mt-2 flex items-end justify-between">
-        <div className="leading-tight font-bold">
-          <Link href={"/"} className="text-black no-underline">
-            <UnderLineText text="Home" />
+      <div className='mt-2 flex items-end justify-between'>
+        <div className='leading-tight font-bold'>
+          <Link href={'/'} className='text-black no-underline'>
+            <UnderLineText text='Home' />
           </Link>
           {menuLinks.map((menuItem) => {
             return (
               <Link
                 key={menuItem.href}
-                className="text-black no-underline"
+                className='text-black no-underline'
                 href={menuItem.href}
-                target="_blank"
+                target='_blank'
               >
                 <UnderLineText text={menuItem.name} />
               </Link>
             );
           })}
-          <UnderLineText text="Team" />
+          <UnderLineText text='Team' />
         </div>
         {/* <div className="w-10 h-10 rounded-full bg-background"></div> */}
       </div>
--- components/svg/svg1.js
+++ components/svg/svg1.js
@@ -1,17 +1,11 @@
-import Image from "next/image";
-
 const RoadMap = ({ imagePath }) => {
   return (
-    <>
-      <div
-        className="bg-contain bg-center bg-no-repeat h-[3000px] md:h-[1000px] w-200% md:w-200%"
-        style={{
-          backgroundImage: `url('${imagePath}')`,
-        }}
-      >
-        {/* Add any other content or elements you want to display within the RoadMap component */}
-      </div>
-    </>
+    <div
+      className='bg-contain bg-center bg-no-repeat h-[3000px] md:h-[1000px] w-200% md:w-200%'
+      style={{
+        backgroundImage: `url('${imagePath}')`,
+      }}
+    ></div>
   );
 };
 
