8443d83d2d4679dd366efdc9f24f1ade28247b88
sk1802 <88622678+sk1802@users.noreply.github.com>
2023-07-30 16:27:59 +0530

    schedule final changes

--- components/svg/svg1.js
+++ components/svg/svg1.js
@@ -1,7 +1,7 @@
 const RoadMap = ({ imagePath }) => {
   return (
     <div
-      className='bg-contain bg-center bg-no-repeat -mt-[45rem] h-[150rem] sm:-mt-[100rem] md:mt-0 sm:h-[300rem] md:h-[100rem]'
+      className='bg-contain bg-center bg-no-repeat -mt-[45rem] h-[200rem] sm:-mt-[100rem] md:mt-0 sm:h-[350rem] md:h-[180rem]'
       style={{
         backgroundImage: `url('${imagePath}')`,
       }}
